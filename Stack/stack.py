b624dfc5c5a8b695a763c90f0e2deb97aba8f7b1
Ng Yew Kong <ngyewkong1996@gmail.com>
2023-11-02 20:31:39 +0800

    impl pop method for stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -35,6 +35,21 @@ class Stack:
             self.top = new_node
         self.height += 1
 
+    # pop method (LIFO so only the top element is popped)
+    def pop(self):
+        # base case if top is None -> return
+        if self.height == 0:
+            return None
+        # case that has nodes to be popped
+        # return the node that is going to be popped
+        # need a variable to hold the node object
+        temp = self.top
+        self.top = self.top.next
+        # remove the pointer of temp.next to remove from original stack
+        temp.next = None
+        self.height -= 1
+        return temp
+
 
 # sample print stack
 print("----- test stack setup class -----")
@@ -45,3 +60,8 @@ print("----- push -----")
 my_stack.push(8)
 my_stack.push(16)
 my_stack.print_stack()  # 16 8 4 (LIFO)
+
+print("----- pop -----")
+print("The node that is being popped from stack is {}".format(
+    my_stack.pop().value))  # 16
+my_stack.print_stack()  # 8 4
