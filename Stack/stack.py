efadd153a8fd005dc1b749ae4108b9c9eb155e6f
Ng Yew Kong <ngyewkong1996@gmail.com>
2023-11-02 20:21:22 +0800

    impl push method for stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -21,8 +21,27 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
+    # push method
+    # rmb the LinkedList impl for Stack the pointer point from top (last element)
+    def push(self, value):
+        new_node = Node(value)
+
+        # base case for no node in the Stack
+        if self.height == 0:
+            self.top = new_node
+        # case to insert node at the end of Stack
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
 
 # sample print stack
 print("----- test stack setup class -----")
 my_stack = Stack(4)
 my_stack.print_stack()  # 4
+
+print("----- push -----")
+my_stack.push(8)
+my_stack.push(16)
+my_stack.print_stack()  # 16 8 4 (LIFO)
