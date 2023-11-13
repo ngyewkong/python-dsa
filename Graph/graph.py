6e85dea31fd2a563932cbd5617418e4c8cd10e48
Ng Yew Kong <ngyewkong1996@gmail.com>
2023-11-14 00:00:25 +0800

    intro to graph - theory

---
+++ Graph/graph.py
@@ -0,0 +1,8 @@
+# Graph Data Structure
+# Vertex (Node) and Edge (Line connecting 2 Vertices)
+# Non-weighted
+# Weighted Edges (Connection with different value attached to it - costs)
+# used in GMaps or Network Routing Protocols
+# Bidirectional (no arrows on edges node-node eg friend follow you, you follow friend on fb)
+# Directional (node->node)
+# LinkedLists are a form of Tree & Tree is a form of graph (Directional Graph)
